6d1cd4d9374e69fae560045dbd8c550a03f026e9
xHue <70556905+slooshied@users.noreply.github.com>
2021-03-14 20:57:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ var url = "https://api.minetools.eu/ping/cometfreebuild.ddns.net/25565";
 $.getJSON(url, function(r) {
     //data is the JSON string
  if(r.error){
-    $('#rest').html('Comet Freebuild is Offline');
+    $('#rest').html('<img src="https://icon-library.com/images/failed-icon/failed-icon-7.jpg" style="width:16px;height:16px;"></img><b>⠀Comet Freebuild is Offline</b>');
    return false;
  } 
 var pl = '';
  if(r.players.sample.length > 0 )
-  $('#rest').html('<b>Players Online: </b>'+r.players.online+pl+'<br>'+'<b>MOTD: </b>'+r.description.replace(/§(.+?)/gi, '')+'<br>');
+  document.getElementById("rest").style.fontFamily = "Open Sans Condensed";
+  $('#rest').html('<img src="https://i.imgur.com/49vJcdL.png" style="width:16px;height:16px;"></img><b>⠀Server is Online</b><br></br><b>Players Online: </b>'+r.players.online+pl+' <b>/</b> 50<br>'+'<b>MOTD: </b>'+r.description.replace(/§(.+?)/gi, '')+'<br>');
  $('#favicon').attr('src', r.favicon);
     
 });
